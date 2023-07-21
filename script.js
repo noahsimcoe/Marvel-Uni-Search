7b1fac6588843dbad935d93fd7d3a2662e4404bb
Dale Talley III <dtalley32@icloud.com>
2023-07-21 03:59:41 -0500

    added queryselectors to javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+// selectors
+var headerSectionClass = document.querySelector("header-section");
+var headerId = document.querySelector("header");
+var searchSectionId = document.querySelector("search-section");
+var searchContainerId = document.querySelector("search-container");
+var btnDivId = document.querySelector("btn-div");
+var searchBtnId = document.querySelector("search-btn");
+var moviesId = document.querySelector("movies");
+var showsId = document.querySelector("shows");
+var comicsId = document.querySelector("comics");
+var gamesId = document.querySelector("games");
+var favoritesId = document.querySelector("favorites");
+var wikiActorsId = document.querySelector("wiki-actors");
