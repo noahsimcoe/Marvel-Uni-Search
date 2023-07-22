7f9f63992d48f83759262111118516503ef3c370
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-07-22 15:35:52 -0500

    initial round of work on the fetch call - stuck on 401 error atm

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,25 @@ var moviesId = document.querySelector("#movies");
 var showsId = document.querySelector("#shows");
 var comicsId = document.querySelector("#comics");
 var gamesId = document.querySelector("#games");
-
 var wikiActorsId = document.querySelector("#wiki-actors");
 var columnIsOneFifthClass = document.querySelector(".column-is-one-fifth");
 var favoritesId = document.querySelector("#favorites");
+
+// required paramaters for the request
+// var heroValue = "Hulk";
+// var baseUrl = "https://gateway.marvel.com/v1/public/characters";
+// var timeStamp = dayjs().unix().toString();
+// var publicKey = "c56260f4d749fa9644507d9cde6f2e8d";
+// var privateKey = "2008ba7cd3708bda8540f651e6375ab1b50070b2";
+// var hash = CryptoJS.MD5(timeStamp, privateKey, publicKey).toString();
+// console.log(hash);
+
+// var apiUrl = `${baseUrl}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&name=${heroValue}`;
+
+// fetch(apiUrl)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         return data;
+//     })
