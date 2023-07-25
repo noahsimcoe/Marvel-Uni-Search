3c068b9484a541c4f2ff4dddfd7aa9c68cdaa34f
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-07-24 20:27:47 -0500

    pulling more data from first API call

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,7 @@ $(function () {
 
     // required paramaters for the request
 
-    var heroValue = "Hulk";
+    var heroValue = "Shang-Chi";
     var baseUrl = "https://gateway.marvel.com/v1/public/characters";
     var timeStamp = dayjs().unix();
     var publicKey = "c56260f4d749fa9644507d9cde6f2e8d";
@@ -40,6 +40,35 @@ $(function () {
             charName = data.data.results[0].name;
             charDescription = data.data.results[0].description;
             charThumbnailUrl = `${data.data.results[0].thumbnail.path}.${data.data.results[0].thumbnail.extension}`;
+
+            comicsList = [];
+            comicsArray = data.data.results[0].comics.items;
+            for (var i = 0; i < comicsArray.length; i++) {
+                comicsList.push(comicsArray[i].name);
+            }
+            comicsList = comicsList.join(", ");
+
+            eventsList = [];
+            eventsArray = data.data.results[0].events.items;
+            for (var i = 0; i < eventsArray.length; i++) {
+                eventsList.push(eventsArray[i].name);
+            }
+            eventsList = eventsList.join(", ");
+
+            storiesList = [];
+            storiesArray = data.data.results[0].stories.items;
+            for (var i = 0; i < storiesArray.length; i++) {
+                storiesList.push(storiesArray[i].name);
+            }
+            storiesList = storiesList.join(", ");
+
+            seriesList = [];
+            seriesArray = data.data.results[0].series.items;
+            for (var i = 0; i < seriesArray.length; i++) {
+                seriesList.push(seriesArray[i].name);
+            }
+            seriesList = seriesList.join(", ");
+
             $(".bio .profile-pic").attr('src', charThumbnailUrl);
             $(".bio .name").text(charName);
             $(".bio .description").text(charDescription);
