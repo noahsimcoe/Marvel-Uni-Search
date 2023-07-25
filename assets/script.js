13cd94cf47c679000ebc4689c2afcde5d943a0ea
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-07-24 20:54:05 -0500

    changes to data that's pulled

--- assets/script.js
+++ assets/script.js
@@ -28,48 +28,6 @@ $(function () {
         heroSearch();
     }
 
-    fetch(apiUrl)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            charName = data.data.results[0].name;
-            charDescription = data.data.results[0].description;
-            charThumbnailUrl = `${data.data.results[0].thumbnail.path}.${data.data.results[0].thumbnail.extension}`;
-
-            comicsList = [];
-            comicsArray = data.data.results[0].comics.items;
-            for (var i = 0; i < comicsArray.length; i++) {
-                comicsList.push(comicsArray[i].name);
-            }
-            comicsList = comicsList.join(", ");
-
-            eventsList = [];
-            eventsArray = data.data.results[0].events.items;
-            for (var i = 0; i < eventsArray.length; i++) {
-                eventsList.push(eventsArray[i].name);
-            }
-            eventsList = eventsList.join(", ");
-
-            storiesList = [];
-            storiesArray = data.data.results[0].stories.items;
-            for (var i = 0; i < storiesArray.length; i++) {
-                storiesList.push(storiesArray[i].name);
-            }
-            storiesList = storiesList.join(", ");
-
-            seriesList = [];
-            seriesArray = data.data.results[0].series.items;
-            for (var i = 0; i < seriesArray.length; i++) {
-                seriesList.push(seriesArray[i].name);
-            }
-            seriesList = seriesList.join(", ");
-
-            $(".bio .profile-pic").attr('src', charThumbnailUrl);
-            $(".bio .name").text(charName);
-            $(".bio .description").text(charDescription);
-        })
     function heroSearch() {
         // required paramaters for the request
         var heroValue = "hulk";
@@ -91,6 +49,35 @@ $(function () {
                 charName = data.data.results[0].name;
                 charDescription = data.data.results[0].description;
                 charThumbnailUrl = `${data.data.results[0].thumbnail.path}.${data.data.results[0].thumbnail.extension}`;
+
+                comicsList = [];
+                comicsArray = data.data.results[0].comics.items;
+                for (var i = 0; i < comicsArray.length; i++) {
+                    comicsList.push(comicsArray[i].name);
+                }
+                comicsList = comicsList.join(", ");
+
+                eventsList = [];
+                eventsArray = data.data.results[0].events.items;
+                for (var i = 0; i < eventsArray.length; i++) {
+                    eventsList.push(eventsArray[i].name);
+                }
+                eventsList = eventsList.join(", ");
+
+                storiesList = [];
+                storiesArray = data.data.results[0].stories.items;
+                for (var i = 0; i < storiesArray.length; i++) {
+                    storiesList.push(storiesArray[i].name);
+                }
+                storiesList = storiesList.join(", ");
+
+                seriesList = [];
+                seriesArray = data.data.results[0].series.items;
+                for (var i = 0; i < seriesArray.length; i++) {
+                    seriesList.push(seriesArray[i].name);
+                }
+                seriesList = seriesList.join(", ");
+
                 $(".bio .profile-pic").attr('src', charThumbnailUrl);
                 $(".bio .name").text(charName);
                 $(".bio .description").text(charDescription);
